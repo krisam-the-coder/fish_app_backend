43807751d0879f9f43964253c87937c2d02829c0
Roshan Acharya <101966707+Roshan-ac@users.noreply.github.com>
2023-06-29 19:23:17 +0545

    fixed buyerRequest

--- src/buyerRequest/buyerRequest.service.ts
+++ src/buyerRequest/buyerRequest.service.ts
@@ -2,54 +2,52 @@ import { db } from "../utils/db.server"
 
 
 type BuyerRequest = {
-  farmerSupplyId :string ,
-  buyerId      :  string ,
-  requestWeight:number,
-  phoneNumber: string
-
+  farmerSupplyId: string,
+  buyerId: string,
+  requestWeight: string,
 };
 
 
-export const createBuyerRequest =async (data:any):Promise<BuyerRequest>=>{
-  const { farmerSupplyId, phoneNumber, buyerId, requestWeight }=data;
-const   createBuyerRequest=await db.buyerRequest.create({
-    data:{
+export const createBuyerRequest = async (data: any): Promise<BuyerRequest> => {
+  const { farmerSupplyId, phoneNumber, buyerId, requestWeight } = data;
+  const createBuyerRequest = await db.buyerRequest.create({
+    data: {
       farmerSupplyId, isApproved: false, phoneNumber, buyerId, requestWeight
     }
   })
   return createBuyerRequest
 }
 
-export const getBuyerRequests =async (id:string):Promise<BuyerRequest[]>=>{
-  
-const  getBuyerRequests=await db.buyerRequest.findMany({ 
-    where:{
-      farmerSupplyId:id
+export const getBuyerRequests = async (id: string): Promise<BuyerRequest[]> => {
+
+  const getBuyerRequests = await db.buyerRequest.findMany({
+    where: {
+      farmerSupplyId: id
     }
   })
   return getBuyerRequests
 }
 
 
-export const approveBuyerRequests =async (id:string):Promise<BuyerRequest>=>{
-  
-  const approveBuyerRequests=await  db.buyerRequest.update({ 
-    where:{
-      buyerId:id
+export const approveBuyerRequests = async (id: string): Promise<BuyerRequest> => {
+
+  const approveBuyerRequests = await db.buyerRequest.update({
+    where: {
+      buyerId: id
     },
-    data:{
-      isApproved:true
+    data: {
+      isApproved: true
     }
   })
   return approveBuyerRequests;
 }
 
 
-export const deleteBuyerRequests =async (id:string):Promise<BuyerRequest>=>{
-  
-  const deleteBuyerRequests =await  db.buyerRequest.delete({ 
-    where:{
-      buyerId:id
+export const deleteBuyerRequests = async (id: string): Promise<BuyerRequest> => {
+
+  const deleteBuyerRequests = await db.buyerRequest.delete({
+    where: {
+      buyerId: id
     }
   })
   return deleteBuyerRequests;
